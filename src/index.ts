cd2a5de205882b58e0568ba4fc8f4e146949f575
Hugo B Tiburtino <hugotiburtino@protonmail.com>
2021-01-17 21:49:59 +0100

    Update license

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 /**
  * This file is part of Serlo.org API
  *
- * Copyright (c) 2020 Serlo Education e.V.
+ * Copyright (c) 2021 Serlo Education e.V.
  *
  * Licensed under the Apache License, Version 2.0 (the "License")
  * you may not use this file except in compliance with the License
@@ -15,12 +15,13 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
- * @copyright Copyright (c) 2020 Serlo Education e.V.
+ * @copyright Copyright (c) 2021 Serlo Education e.V.
  * @license   http://www.apache.org/licenses/LICENSE-2.0 Apache License 2.0
  * @link      https://github.com/serlo-org/api.serlo.org for the canonical source repository
  */
 import fs from 'fs'
 import path from 'path'
+
 import { CacheWorker } from './cache-worker'
 
 void start()
@@ -42,11 +43,11 @@ async function start() {
     service: process.env.SERVICE,
     pagination,
   })
-  
+
   const cacheKeysPath = path.join(__dirname, 'cache-keys.json')
   const data = fs.readFileSync(cacheKeysPath, 'utf8')
   const cacheKeys = JSON.parse(data) as string[]
-  
+
   const { errorLog } = await cacheWorker.update(cacheKeys)
 
   if (cacheWorker.hasSucceeded()) {
